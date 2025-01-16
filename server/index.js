1e104486622bb55451f583c7a6eece1b937fc7c5
Chotu Kumar <ck4001252@gmail.com>
2025-01-16 15:26:51 +0530

    cors allowed from origin

--- server/index.js
+++ server/index.js
@@ -12,7 +12,13 @@ dotenv.config();
 
 connectDB();
 
-app.use(cors())
+app.use(cors(
+    {
+        origin: '*', 
+        credentials: true, 
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+      }
+))
 app.use(express.json())
 app.use(morgan('dev'))
 
